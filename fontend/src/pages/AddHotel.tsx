f4a234f7b9bfd58d704dedf5a02342d3fbc40fae
nguyenducquanglinh <linh35889@gmail.com>
2025-02-11 23:30:01 +0700

    commit

---
+++ fontend/src/pages/AddHotel.tsx
@@ -0,0 +1,28 @@
+import { useMutation } from "@tanstack/react-query";
+import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
+import * as apiClient from "../api-client";
+
+const AddHotel = () => {
+  const { showToast } = useAppContext();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: apiClient.addMyHotel,
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error Saving Hotel", type: "ERROR" });
+    },
+  });
+  
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return <ManageHotelForm onSave={handleSave} isLoading={isPending} />
+  ;
+};
+
+export default AddHotel;
---
+++ fontend/src/pages/EditHotel.tsx
@@ -0,0 +1,39 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import * as apiClient from "../api-client";
+import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
+
+const EditHotel = () => {
+  const { hotelId } = useParams();
+  const { showToast } = useAppContext();
+
+  const { data: hotel } = useQuery({
+    queryKey: ["fetchMyHotelById", hotelId],
+    queryFn: () => apiClient.fetchMyHotelById(hotelId || ""),
+    enabled: !!hotelId,
+  });
+  
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: apiClient.updateMyHotelById,
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error Saving Hotel", type: "ERROR" });
+    },
+  });
+  
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return (
+    <ManageHotelForm hotel={hotel} onSave={handleSave} isLoading={isPending} />
+
+  );
+};
+
+export default EditHotel;
